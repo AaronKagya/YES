5690234f72be65fb57408c0ab3919c484a39cbde
Aaron Kag-ya <erinkagya@gmail.com>
2025-02-06 13:54:21 +0300

    Add files via upload

---
+++ teaExample.js
@@ -0,0 +1,16 @@
+//Syncchronous Programming
+//step 1
+console.log('boiling tea');
+//step 2
+console.log('adding sugar');
+//step 3
+console.log('adding tea leaves');
+
+// Here the process is step by step, one after the other
+
+// //Asynchronous Programming
+// //step 1
+console.log('boiling water');
+// //step 2
+console.log('adding sugar');
+// //step 3
