db901c76cc537f515dbcc5e6d027e1cf996977e1
Aaron Kag-ya <erinkagya@gmail.com>
2025-03-24 10:20:50 +0300

    Update fvg.py

--- fvg.py
+++ fvg.py
@@ -9,7 +9,7 @@ if not mt5.initialize():
     quit()
 
 # Define FVG strategy parameters
-symbol = "EURUSD"
+symbol = "Jump index 50"
 timeframe = mt5.TIMEFRAME_M15
 fvg_threshold = 10  # Example threshold, adjust as needed
 
